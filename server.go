9719d808359d0a1282a507742f4e211485eec92d
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2020-03-17 21:21:47 -0700

    add docker compose

--- server.go
+++ server.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
 	})
-	r.Run(":3000")
+
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
+	})
+
+	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["name"]
+
+		fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
+	})
+
+	http.ListenAndServe(":80", r)
 }
